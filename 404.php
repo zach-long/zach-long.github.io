3512687f9f762d3e9fec36b7ce71581883dc79c3
Zachary Long <zach.writes.code@gmail.com>
2016-06-16 19:21:10 -0400

    Temporary

---
+++ 404.php
@@ -0,0 +1,11 @@
+<DOCTYPE html>
+<html>
+  <body>
+    I'm working on this. (6/16)
+    <br><br>
+    Here are the relevant links to my works,
+    <br>
+    no frivolities today.
+  </body>
+</html>
+
